aff1f7bc1f9fa074e04a1336cc1ca7f018c75197
Aporvachaudhary123 <chaudharyapurva123@gmail.com>
2026-02-02 16:50:30 +0530

    Edit

--- basic-programs/AreaOfCircle.java
+++
@@ -1,7 +0,0 @@
-public class AreaOfCircle {
-    public static void main(String[] args) {
-        double radius = 5.0;           // radius of circle
-        double area = Math.PI * radius * radius;   // area formula
-        System.out.println("Area of the circle is: " + area);
-    }
-}
